20b4ff3dbc29496728b8a358b3d805772bc58651
Commander Kracok <kracok@gmail.com>
2020-04-22 10:39:18 +0200

    opdracht 8

---
+++ opdracht_008_casten_van_variablen.py
@@ -0,0 +1,14 @@
+preiPrijs = "2"
+
+print("Prei kost ", preiPrijs, " euro per kilo")
+
+preiAantal = 4
+
+print(preiAantal * int(preiPrijs))
+
+broccoliPrijs = "234"
+
+broccoliAantal = "160"
+
+print((int(broccoliAantal) / 100), "kilo broccoli kost",
+      round((int(broccoliPrijs) * int(broccoliAantal)) / 10000, 2))
