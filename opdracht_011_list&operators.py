11fd7a9ad77163e2876183a5c4ef145ed1969d62
kracok <kracok@gmail.com>
2020-04-28 11:22:56 +0200

    opdracht 11

---
+++ opdracht_011_list&operators.py
@@ -0,0 +1,207 @@
+shane_embury = [
+    "Shane Embury",
+    ["bass",
+     "backing vocals"],
+    [1987,
+     2021]
+    ]
+
+mark_greenway = [
+    "Mark \"Barney\"Greenway",
+    "lead vocals",
+    [[1989,
+    1996],
+    [1997,
+    2021]]
+    ]
+
+danny_herrera = [
+    "Danny Herrera",
+    "drums",
+    [1991,
+    2021]
+    ]
+
+mitch_harris = [
+    "Mitch Harris",
+    ["guitars",
+    "backing vocals"],
+    [1990,
+    2014]
+    ]
+
+nicholas_bullen = [
+    "Nicholas \"Nik Napalm\" Bullen",
+    ["lead vocals",
+    "bass"],
+    [1981,
+    1986]
+    ]
+
+miles_ratledge = [
+    "Miles \"Rat\" Ratledge",
+    "drums",
+    [1981,
+    1985]
+    ]
+
+simon_oppenheimer = [
+    "Simon \"Si O\" Oppenheimer",
+    "guitars",
+    [1981,
+    1982]
+    ]
+
+graham_robertson = [
+    "Graham \"Grayhard\" Robertson",
+    ["guitars",
+    "bass"],
+    [1982,
+    1985]
+    ]
+
+daryl_fedeski = [
+    "Daryl \"Daz F\" Fedeski",
+    "guitars",
+    1982
+    ]
+
+finbar_quinn = [
+    "Finbar \"Fin\" Quinn",
+    "bass",
+    [1983,
+    1984]
+    ]
+
+marian_williams = [
+    "Marian Williams",
+    "lead vocals",
+    1984
+    ]
+
+damien_errington = [
+    "Damien Errington",
+    "guitars",
+    1985
+    ]
+
+justin_broadrick = [
+    "Justin Broadrick",
+    ["guitars",
+    "backing and lead vocals"],
+    [1985,
+    1986]
+    ]
+
+peter_shaw = [
+    "Peter \"P-Nut\" Shaw",
+    "bass",
+    1985
+    ]
+
+mick_harris = [
+    "Mick Harris",
+    ["drums",
+    "backing vocals"],
+    [1985,
+    1991]
+    ]
+
+jim_whitely = [
+    "Jim Whitely",
+    "bass",
+    [1986,
+    1987]
+    ]
+
+frank_healy = [
+    "Frank Healy",
+    "guitars",
+    1986
+    ]
+
+bill_steer = [
+    "Bill Steer",
+    "guitars",
+    [1987,
+    1989]
+    ]
+
+lee_dorian = [
+    "Lee Dorrian",
+    "lead vocals",
+    [1987,
+    1989]
+    ]
+
+jesse_pintado = [
+    "Jesse Pintado",
+    "guitars",
+    [1989,
+    2004]
+    ]
+
+phil_vane = [
+    "Phil Vane",
+    "lead vocals",
+    [1996,
+    1997]
+    ]
+
+erik_burke = [
+    "Erik Burke",
+    "guitars",
+    2015
+    ]
+
+jesper_liverod = [
+    "Jesper Liveröd",
+    "bass",
+    2017
+    ]
+
+
+#1981
+print("Het jaar is", nicholas_bullen[2][0], ",",
+      nicholas_bullen[0], "doet de", nicholas_bullen[1][0], "en de", nicholas_bullen[1][1], ".",
+      miles_ratledge[0], "doe de", miles_ratledge[1], "en",
+      simon_oppenheimer[0], "doet de", simon_oppenheimer[1], ".")
+
+#1982
+print("Het jaar is", graham_robertson[2][0], ",",
+      nicholas_bullen[0], "doet de", nicholas_bullen[1][0], "en de", nicholas_bullen[1][1], ".",
+      miles_ratledge[0], "doe de", miles_ratledge[1], ",",
+      simon_oppenheimer[0], "doet de", simon_oppenheimer[1], ",",
+      graham_robertson[0], "doet de", graham_robertson[1][0], "en de", graham_robertson[1][1], "en",
+      daryl_fedeski[0], "doet de", daryl_fedeski[1], ".")
+
+#1983
+print("Het jaar is", finbar_quinn[2][0], ",",
+      nicholas_bullen[0], "doet de", nicholas_bullen[1][0], "en de", nicholas_bullen[1][1], ".",
+      miles_ratledge[0], "doe de", miles_ratledge[1], ",",
+      graham_robertson[0], "doet de", graham_robertson[1][0], "en de", graham_robertson[1][1], ",",
+      daryl_fedeski[0], "doet de", daryl_fedeski[1], "en",
+      finbar_quinn[0], "doet de", finbar_quinn[1], ".")
+
+#1984
+print("Het jaar is", finbar_quinn[2][0], ",",
+      nicholas_bullen[0], "doet de", nicholas_bullen[1][0], "en de", nicholas_bullen[1][1], ".",
+      miles_ratledge[0], "doe de", miles_ratledge[1], ",",
+      graham_robertson[0], "doet de", graham_robertson[1][0], "en de", graham_robertson[1][1], ",",
+      daryl_fedeski[0], "doet de", daryl_fedeski[1], ",",
+      finbar_quinn[0], "doet de", finbar_quinn[1], "en",
+      marian_williams[0], "doet de", marian_williams[1])
+
+#1985
+print("Het jaar is", finbar_quinn[2][0], ",",
+      nicholas_bullen[0], "doet de", nicholas_bullen[1][0], "en de", nicholas_bullen[1][1], ".",
+      miles_ratledge[0], "doe de", miles_ratledge[1], ",",
+      graham_robertson[0], "doet de", graham_robertson[1][0], "en de", graham_robertson[1][1], ",",
+      daryl_fedeski[0], "doet de", daryl_fedeski[1], ",",
+      marian_williams[0], "doet de", marian_williams[1], ",",
+      damien_errington[0], "doet de", damien_errington[1], ",",
+      justin_broadrick[0], "doet de", justin_broadrick[1][0], "en de",justin_broadrick[1][1], ",",
+      peter_shaw[0], "doet de", peter_shaw[1], "en",
+      mick_harris[0], "doet de", mick_harris[1][0], "en de",mick_harris[1][1], ".")
+
+#enz. ik kan wel doorgaan met elk jaar maar dat is erg veel typen en ik snap de oefening
